2d129fca6f76f4d4198347aec833ac3a3637294a
Tino Sabetta <tino.sabetta@gmail.com>
2021-06-05 17:37:12 +0200

    quick fix to make animation smoother

--- js/app.js
+++ js/app.js
@@ -2,30 +2,11 @@ var app = (function() {
     /* --------------------------------------------------------------------------------------------------
     Variables
     ---------------------------------------------------------------------------------------------------*/
-    var words = [{
-            "list": ["Das Œuvre", "Das Substrat", "Die Rhythmisierung", "Der Pinselduktus", "Der Mythos", "Die Linearität", "Die Komposition", "Die Impression", "Die Idealisierung", "Der Gestus", "Der Farbauftrag", "Die Expression", "Die Atmosphäre", "Die Abstraktion", "Die Ästhetik"],
-            "index": function() {
-                return getRndInteger(0, this.list.length - 1);
-            },
-        },
-        {
-            "list": ["transzendiert", "collagiert", "illuminiert", "intendiert", "evoziert", "manifestiert", "moduliert", "reflektiert", "suggeriert", "thematisiert", "transformiert", "reproduziert", "verhandelt", "zitiert", "simuliert"],
-            "index": function() {
-                return getRndInteger(0, this.list.length - 1);
-            },
-        },
-        {
-            "list": ["virtuell", "tautologisch", "informell", "surreal", "stringent", "seriell", "mimetisch", "narrativ", "hypothetisch", "haptisch", "fragmentarisch", "hermetisch", "figurativ", "experimentell", "assoziativ"],
-            "index": function() {
-                return getRndInteger(0, this.list.length - 1);
-            },
-        },
-        {
-            "list": ["das Artifizielle", "das Auratische", "den Bildraum", "das Eingeschriebene", "das Erratische", "das Existentielle", "die Ikonographie", "das Motivische", "das Orgiastische", "die Materialität", "das Phantastische", "die Relativität", "das Selbstreflexive", "das Kosmische", "die Inszenierung"],
-            "index": function() {
-                return getRndInteger(0, this.list.length - 1);
-            },
-        },
+    var words = [
+        ["Das Œuvre", "Das Substrat", "Die Rhythmisierung", "Der Pinselduktus", "Der Mythos", "Die Linearität", "Die Komposition", "Die Impression", "Die Idealisierung", "Der Gestus", "Der Farbauftrag", "Die Expression", "Die Atmosphäre", "Die Abstraktion", "Die Ästhetik"],
+        ["transzendiert", "collagiert", "illuminiert", "intendiert", "evoziert", "manifestiert", "moduliert", "reflektiert", "suggeriert", "thematisiert", "transformiert", "reproduziert", "verhandelt", "zitiert", "simuliert"],
+        ["virtuell", "tautologisch", "informell", "surreal", "stringent", "seriell", "mimetisch", "narrativ", "hypothetisch", "haptisch", "fragmentarisch", "hermetisch", "figurativ", "experimentell", "assoziativ"],
+        ["das Artifizielle", "das Auratische", "den Bildraum", "das Eingeschriebene", "das Erratische", "das Existentielle", "die Ikonographie", "das Motivische", "das Orgiastische", "die Materialität", "das Phantastische", "die Relativität", "das Selbstreflexive", "das Kosmische", "die Inszenierung"]
     ];
     /* --------------------------------------------------------------------------------------------------
     functions
@@ -52,24 +33,24 @@ var app = (function() {
         var counter = 1;
         var animation = setInterval(shuffleParas, 50);
         var index = [];
-        index[0] = words[0].index();
-        index[1] = words[1].index();
-        index[2] = words[2].index();
-        index[3] = words[3].index();
+        index[0] = getRndInteger(0, words[0].length-1);
+        index[1] = getRndInteger(0, words[1].length-1);
+        index[2] = getRndInteger(0, words[2].length-1);
+        index[3] = getRndInteger(0, words[3].length-1);
 
         function shuffleParas() {
             if (counter >= 10) {
                 clearInterval(animation);
-                paragraphs[0].textContent = words[0].list[index[0]];
-                paragraphs[1].textContent = words[1].list[index[1]];
-                paragraphs[2].textContent = words[2].list[index[2]];
-                paragraphs[3].textContent = words[3].list[words[3].index()] + ".";
+                paragraphs[0].textContent = words[0][index[0]];
+                paragraphs[1].textContent = words[1][index[1]];
+                paragraphs[2].textContent = words[2][index[2]];
+                paragraphs[3].textContent = words[3][index[3]] + ".";
             }
             else {
-                paragraphs[0].textContent = words[0].list[index[0]].shuffle().trim();
-                paragraphs[1].textContent = words[1].list[index[1]].shuffle().trim();
-                paragraphs[2].textContent = words[2].list[index[2]].shuffle().trim();
-                paragraphs[3].textContent = words[3].list[index[3]].shuffle().trim() + ".";
+                paragraphs[0].textContent = words[0][index[0]].shuffle().trim();
+                paragraphs[1].textContent = words[1][index[1]].shuffle().trim();
+                paragraphs[2].textContent = words[2][index[2]].shuffle().trim();
+                paragraphs[3].textContent = words[3][index[3]].shuffle().trim() + ".";
                 counter++;
             }
         }
